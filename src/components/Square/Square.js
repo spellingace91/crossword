d786d41f9601ebb50e997c083043a68f4955f981
spellingace91 <spellingace91@gmail.com>
2019-02-02 14:21:59 -0500

    Add arrow key and backspace functions
    Allow the user to traverse the grid using arrow keys
    
    Remove previous letter by pressing backspace only (instead of having to click into the square, position the cursor, and press backspace)

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -18,9 +18,7 @@ const square = (props) => {
         maxLength="1"
         tabIndex={props.tabindex}
         pattern="[a-zA-Z]+"
-        onFocus={(event) => props.onFocus(event, props.index)}
         onKeyDown={(event) => props.onKeyDown(event, props.index)}
-        onChange = {(event) => props.onChange(event, props.index)}
         index={props.index}
         >
       </input>
--- src/containers/CrosswordGrid/CrosswordGrid.js
+++ src/containers/CrosswordGrid/CrosswordGrid.js
@@ -5,6 +5,7 @@ import classes from './CrosswordGrid.module.css';
 class CrosswordGrid extends Component {
   state = {
     solution: this.props.solution,
+    hasStarted: false,
     isCompleted: false,
   }
 
@@ -39,35 +40,137 @@ class CrosswordGrid extends Component {
         }
       }
     }
-
-    console.log(grid.flat().some((square) => !square.correct));
+    
     return grid;
   }
 
-  onFocusHandler = (event, index) => {
-  }
-
   onKeyDownHandler = (event, index) => {
-  }
-
-  onChangeHandler = (event, index) => {
-    let [row, column] = index.split(',');
-    if (this.crosswordGrid[row][column].letter === event.target.value.toUpperCase()) {
-      this.crosswordGrid[row][column].correct = true;
+    if (!this.state.hasStarted) {
+      // start timer
+      this.setState({hasStarted: true});
     }
 
+    const key = event.key;
+    let [row, column] = index.split(',');
+    row = +row;
+    column = +column;
+    const crosswordGrid = this.crosswordGrid;
+    const flatGrid = crosswordGrid.flat();
+    let nextSquareGrid = [];
+    let nextSquare = {};
+
+    crosswordGrid[row][column].correct = crosswordGrid[row][column].letter === key.toUpperCase();
+
     // determine if the puzzle has been solved
-    if (this.crosswordGrid.flat().every((square) => square.correct)) {
+    if (crosswordGrid.flat().every((square) => square.correct)) {
       this.setState({isCompleted: true});
       alert('You finished the puzzle!');
     }
 
-    const flatGrid = this.crosswordGrid.flat();
-    const nextSquareGrid = flatGrid.slice((+row) * Math.sqrt(flatGrid.length) + (+column) + 1);
-    const nextSquare = nextSquareGrid.find((square) => square.letter);
-    
-    if (nextSquare) {
-      document.querySelector(`input[index="${nextSquare.row},${nextSquare.column}"]`).focus();
+    // if a letter was pressed
+    if (key && /^[a-zA-Z]{1}$/.test(key)) {
+      event.target.value = key.toUpperCase();
+      nextSquareGrid = flatGrid.slice(
+        row * Math.sqrt(flatGrid.length) + column + 1
+      );
+      nextSquare = nextSquareGrid.find((square) => square.letter);
+      
+      if (nextSquare) {
+        document.querySelector(
+          `input[index="${nextSquare.row},${nextSquare.column}"]`)
+          .focus();
+      }
+
+      event.preventDefault();
+    }
+    // if navigation keys were pressed
+    else {
+      switch (event.nativeEvent.keyCode) {
+        case 8: // backspace
+          event.target.value = '';
+          break;
+        case 9: // tab
+          // TODO: implement word grouping
+          break;
+        case 32: // space bar
+          break;
+        case 37: // left arrow
+          if (row === 0 && column === 0) {
+            document.querySelector(`input[index="${crosswordGrid.length - 1},${crosswordGrid.length - 1}"]`).focus();
+          }
+          else {
+            nextSquareGrid = flatGrid.slice(
+              0, row * Math.sqrt(flatGrid.length) + column
+            ).filter((square) => square.letter);
+            nextSquare = nextSquareGrid[nextSquareGrid.length - 1];
+
+            if (nextSquare) {
+              document.querySelector(
+                `input[index="${nextSquare.row},${nextSquare.column}"]`).focus();
+            }
+          }
+          break;
+        case 38: // up arrow
+          // if we're at the top of the grid, we move to the last row
+          const targetRow = row === 0 ? crosswordGrid.length - 1 : row - 1;
+
+          nextSquareGrid = flatGrid.slice(
+            0, targetRow * Math.sqrt(flatGrid.length) + column + 1
+          ).filter((square) =>
+            square.letter && square.column === column);
+          // if there are no letter squares above (just blanks), wrap around
+          if (nextSquareGrid.length === 0) {
+            nextSquareGrid = flatGrid.slice(
+              row * Math.sqrt(flatGrid.length) + column
+            ).filter((square) => square.letter && square.column === column);
+          }
+          nextSquare = nextSquareGrid[nextSquareGrid.length - 1];
+
+          if (nextSquare) {
+            document.querySelector(
+              `input[index="${nextSquare.row},${nextSquare.column}"]`).focus();
+          }
+          break;
+        case 39: // right arrow
+          if (row === crosswordGrid.length - 1 
+                && column === crosswordGrid.length - 1) {
+                  document.querySelector(`input[index="0,0"]`).focus();
+                }
+          else {
+            nextSquareGrid = flatGrid.slice(
+              row * Math.sqrt(flatGrid.length) + column + 1
+            );
+            nextSquare = nextSquareGrid.find((square) => square.letter);
+
+            if (nextSquare) {
+              document.querySelector(
+                `input[index="${nextSquare.row},${nextSquare.column}"]`).focus();
+            }
+          }
+          break;
+        case 40: // down arrow
+          const targetRowDown = (row === crosswordGrid.length - 1) ? 0 : row + 1;
+
+          nextSquareGrid = flatGrid.slice(
+            targetRowDown * Math.sqrt(flatGrid.length) + column
+          ).filter((square) => 
+            square.letter && square.column === column);
+          // if there are no letter square below (just blanks), wrap around
+          if (nextSquareGrid.length === 0) {
+            nextSquareGrid = flatGrid.filter((square) => 
+              square.letter && square.column === column
+            );
+          }
+          nextSquare = nextSquareGrid[0];
+
+          if (nextSquare) {
+            document.querySelector(
+              `input[index="${nextSquare.row},${nextSquare.column}"]`).focus();
+          }
+          break;
+        default:
+          break;
+      }
     }
   }
 
@@ -89,9 +192,7 @@ class CrosswordGrid extends Component {
                       blank={!letter}
                       tabindex={!letter ? '-1' : '1'}
                       numbering={square.number}
-                      onFocus={this.onFocusHandler}
                       onKeyDown={this.onKeyDownHandler}
-                      onChange={this.onChangeHandler}
                       index={`${row},${column}`}
                       key={`${row},${column}`}
                     />
